fa4e33fd3579022f401e40b641c946d3e9601618
Jordan <jordanotk@gmail.com>
2021-05-21 16:57:47 +0800

    Minor

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 var db= {
-host: "localhost",
-database:'estic',
-user:'itp211',
-password:'itp211',
+    host: "localhost",
+    database:'estic',
+    user:'itp211',
+    password:'itp211',
 }
 export default db;
