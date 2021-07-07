6a075be21f71025f1457b84a622afba41d514f1d
Yukiua <66011734+Yukiua@users.noreply.github.com>
2021-07-07 16:13:37 +0800

    additional routing for settings

--- routes/Performer.mjs
+++ routes/Performer.mjs
@@ -16,13 +16,12 @@ const regexName = /^[a-zA-Z][a-zA-Z]{2,}$/;
 //	Min 8 character, 1 upper, 1 lower, 1 number, 1 symbol
 const regexPwd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
+import SettingsOptions from './PerformerSettings.mjs'
+router.use("/settings", SettingsOptions)
+
 router.get("/dashboard", ensureAuthenticated, dashboard_page);
 router.get("/analytics", ensureAuthenticated, analytics_page);
 router.get("/settings",ensureAuthenticated, settings_page);
-router.get("/upload", ensureAuthenticated, upload_page);
-router.get("/update", ensureAuthenticated, update_page);
-router.post("/update", update_process);
-router.get("/delete", ensureAuthenticated,delete_page);
 router.get("/logout", logout_page);
 router.get("/createLivestream", ensureAuthenticated, createLive_page);
 
@@ -64,120 +63,6 @@ async function settings_page(req,res){
 	return res.render('performer/settings.html', {
 		name: performer.name
 	})
-};
-
-async function upload_page(req,res){
-	console.log("Upload page accessed");
-	let email = req.cookies['performer']
-	const performer = await Performer.findOne({
-		where: { email: email }
-	})
-	return res.render('performer/upload.html', {
-		name: performer.name
-	})
-};
-
-async function delete_page(req,res){
-	console.log("Performer Delete accessed");
-	let email = req.cookies['performer']
-	const performer = await Performer.findOne({
-		where: { email: email }
-	})
-	return res.render('performer/delete.html', {
-		name: performer.name
-	})
-};
-
-async function update_page(req, res) {
-	console.log("Performer Update accessed");
-	let email = req.cookies['performer']
-	const performer = await Performer.findOne({
-		where: { email: email }
-	})
-	return res.render('performer/update.html', {
-		name: performer.name,
-		email: performer.email
-	})
-};
-async function update_process(req, res) {
-	console.log("Update contents received");
-	console.log(req.body);
-	let email = req.cookies['performer']
-	const performer = await Performer.findOne({
-		where: { email: email }
-	})
-	let errors = [];
-	//	Check your Form contents
-	//	Basic IF ELSE STUFF no excuse not to be able to do this alone
-	//	Common Sense
-	try {
-		if (!regexName.test(req.body.name)) {
-			errors = errors.concat({ text: "Invalid name provided! It must be minimum 3 characters and starts with a alphabet." });
-		}
-		if (!regexEmail.test(req.body.email)) {
-			errors = errors.concat({ text: "Invalid email address!" });
-		}
-		if (req.body.password != '') {
-			if (!regexPwd.test(req.body.password)) {
-				errors = errors.concat({ text: "Password requires minimum eight characters, at least one uppercase letter, one lowercase letter and one number and one symbol!" });
-			}
-		}
-		if (errors.length > 0) {
-			throw new Error("There are validation errors");
-		}
-	}
-	catch (error) {
-		console.error("There is errors with the update form body.");
-		console.error(error);
-		console.log("smth weird")
-		return res.render('performer/update.html', { errors: errors });
-	}
-	try {
-		if (req.body.name == '') {
-			req.body.name = performer.name
-		}
-		if (req.body.email == '') {
-			req.body.email = performer.email
-		}
-		if (req.body.password == '') {
-			req.body.password = performer.password
-		}
-		else {
-			req.body.password = Hash.sha256().update(req.body.password).digest("hex")
-		}
-		console.log("b4 insert")
-		Performer.update({
-			name: req.body.name,
-			email: req.body.email,
-			password: req.body.password
-		}, {
-			where: {
-				id: performer.id,
-			}
-		})
-		User.update({
-			name: req.body.name,
-			email: req.body.email,
-			password: req.body.password
-		}, {
-			where: {
-				id: performer.id,
-			}
-		})
-			.then(() => {
-				console.log('insert')
-				flashMessage(res, 'success', 'Successfully created an account. Please login', 'fas fa-sign-in-alt', true);
-				console.log(req.body)
-				res.cookie('performer', req.body.email, { maxAge: 900000, httpOnly: true });
-				return res.redirect("../performer/dashboard");
-			}).catch(err => console.log(err))
-	}
-	catch (error) {
-		//	Else internal server error
-		console.error(`Failed to update user: ${req.body.email} `);
-		console.error(error);
-		return res.status(500).end();
-	}
 };
 
 async function logout_page(req, res) {
