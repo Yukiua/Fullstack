1786a615cad5bf69e2b0e747b3b0417a460a1df1
Jordan <jordanotk@gmail.com>
2021-07-07 23:50:15 +0800

    mjs

--- routes/Concert.mjs
+++ routes/Concert.mjs
@@ -1,11 +1,20 @@
 import route from 'express';
-import Concert from '../models/Concert.js';
-import CookieParser    from 'cookie-parser';
-
-
 const { Router } = route;
 const router = Router();
+import Concert from '../models/Concert.js';
+import CookieParser    from 'cookie-parser';
 
 router.get("/table", table);
 router.get("/table-data", table_data);
 
+async function table(req, res){
+    return res.render("concertList");
+}
+
+async function table_data(req, res){
+    const concerts = await ModelConcert.findAll({raw: true});
+    return res.json({
+        "total": concerts.length,
+        "rows": concerts
+    })
+}
